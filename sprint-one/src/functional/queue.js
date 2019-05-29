b8ac1fcc2f38f2d9f5c2203227a8f3ab6297ec6d
Elinex <eline.ax@gmail.com>
2019-05-29 15:26:44 -0700

    Implement partially Queue function to Functional Pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,13 +6,40 @@ var Queue = function() {
 
   // Implement the methods below
 
+  var count = 0;
+
   someInstance.enqueue = function(value) {
+    // creates a new key-value
+    // add this new property at the end of the queue (storage)
+    storage[count] = value
+    console.log('storage after enqueue: ', storage);
+
+    // add 1 to count
+    count ++;
+
+    // returns storage
+    return storage
   };
 
   someInstance.dequeue = function() {
+
+    // first key
+    var firstkey = Object.keys(storage)[0]
+
+    // removes the first property of storage
+    delete storage[firstkey]
+    console.log('storage after dequeue: ', storage);
+
+    return storage
   };
 
   someInstance.size = function() {
+    // get the number of keys in the storage
+    var keysNumber = Object.keys(storage).length
+    console.log('storage size: ', keysNumber);
+
+    // return number of keys of storage
+    return keysNumber
   };
 
   return someInstance;
