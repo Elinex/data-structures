d376e029ee1c9baa34aca669c20a7bc8cdb90d9a
Elinex <eline.ax@gmail.com>
2019-05-31 13:23:53 -0700

    Add this keyword for queue in functional shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,28 +1,28 @@
 var Queue = function(){
   var someInstance = {};
+  someInstance.storage = {};
   _.extend(someInstance, queueMethods);
   return someInstance;
 };
 
 var queueMethods = {};
-var storage = {};
 
 queueMethods.enqueue = function(value){
-  var keys = Object.keys(storage);
+  var keys = Object.keys(this.storage);
   var maxKey = Math.max(Number(...keys));
   var newKey = maxKey + 1;
-  storage[newKey] = value;
+  this.storage[newKey] = value;
 };
 
 queueMethods.dequeue = function() {
-  var keys = Object.keys(storage);
+  var keys = Object.keys(this.storage);
   var minKey = Math.min(Number(...keys));
-  var value = storage[minKey];
-  delete storage[minKey];
+  var value = this.storage[minKey];
+  delete this.storage[minKey];
   return value;
 };
 
 queueMethods.size = function() {
-  var keysNumber = Object.keys(storage).length;
+  var keysNumber = Object.keys(this.storage).length;
   return keysNumber;
 };
