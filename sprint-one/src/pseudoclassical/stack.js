89ec15b15ea0a751a5fcfbd41bb59abf9497b273
Elinex <eline.ax@gmail.com>
2019-05-31 00:07:59 -0700

    Implement stack for PseudoClassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,34 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function(){
+  this;
 };
 
+var storage = {};
 
+Stack.prototype.push = function(value){
+  var keys = Object.keys(storage);
+  var maxKey = Math.max(Number(...keys));
+  var newKey = maxKey + 1;
+  storage[newKey] = value;
+};
+
+Stack.prototype.pop = function(){
+  var keys = Object.keys(storage);
+  var stringToNumber = function(){
+    var toNumber = [];
+    for (var i =0 ; i < keys.length; i ++){
+      toNumber.push(Number(keys[i]));
+    }
+    return toNumber;
+  }();
+  var maxKey = Math.max(...stringToNumber);
+  var value = storage[maxKey];
+  delete storage[maxKey];
+  return value;
+};
+
+Stack.prototype.size = function(){
+  var keysNumber = Object.keys(storage).length;
+  return keysNumber;
+};
+
+var someInstance = new Stack();
