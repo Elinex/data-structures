ca11eaad01c7ed0c217d751dad684d6b77bac075
Elinex <eline.ax@gmail.com>
2019-05-30 23:49:32 -0700

    Insert _.extend method to queue functional

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,46 +1,28 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function(){
   var someInstance = {};
-  someInstance.enqueue = queueMethods.enqueue;
-  someInstance.dequeue = queueMethods.dequeue;
-  someInstance.size = queueMethods.size;
-
+  _.extend(someInstance, queueMethods);
   return someInstance;
-
 };
 
 var queueMethods = {};
 var storage = {};
 
 queueMethods.enqueue = function(value){
-  // create a numeric key
   var keys = Object.keys(storage);
   var maxKey = Math.max(Number(...keys));
   var newKey = maxKey + 1;
-
-  // creates a new key-value
-  // add new property at the end of the queue (storage)
   storage[newKey] = value;
 };
 
-queueMethods.dequeue = function(){
-  // get the first key in the storage
+queueMethods.dequeue = function() {
   var keys = Object.keys(storage);
   var minKey = Math.min(Number(...keys));
-
-  // "value" is just to keep the value reference, to be returned after
-  // delete storage[minkey] happen
   var value = storage[minKey];
-
   delete storage[minKey];
-
   return value;
 };
 
-queueMethods.size = function(){
-  // get the number of keys in the storage
+queueMethods.size = function() {
   var keysNumber = Object.keys(storage).length;
-
   return keysNumber;
 };
