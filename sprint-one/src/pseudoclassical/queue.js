65b080fd8584a5087fce9674f979d0096f831c6e
Elinex <eline.ax@gmail.com>
2019-05-31 00:05:45 -0700

    Implement stack for PseudoClassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function(){
+  this;
 };
 
+var storage = {};
 
+Queue.prototype.enqueue = function(value){
+  var keys = Object.keys(storage);
+  var maxKey = Math.max(Number(...keys));
+  var newKey = maxKey + 1;
+  storage[newKey] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  var keys = Object.keys(storage);
+  var minKey = Math.min(Number(...keys));
+  var value = storage[minKey];
+  delete storage[minKey];
+  return value;
+};
+
+Queue.prototype.size = function() {
+  var keysNumber = Object.keys(storage).length;
+  return keysNumber;
+};
+
+var someInstance = new Queue();
