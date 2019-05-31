0f10aebb6f83572e4f9a648963130117b1277cd6
Elinex <eline.ax@gmail.com>
2019-05-30 23:58:59 -0700

    Implement stack for prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function(){
+  var someInstance = Object.create(stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
+var storage = {};
 
+stackMethods.push = function(value){
+  var keys = Object.keys(storage);
+  var maxKey = Math.max(Number(...keys));
+  var newKey = maxKey + 1;
+  storage[newKey] = value;
+};
 
+stackMethods.pop = function(){
+  var keys = Object.keys(storage);
+  var stringToNumber = function(){
+    var toNumber = [];
+    for (var i =0 ; i < keys.length; i ++){
+      toNumber.push(Number(keys[i]));
+    }
+    return toNumber;
+  }();
+  var maxKey = Math.max(...stringToNumber);
+  var value = storage[maxKey];
+  delete storage[maxKey];
+  return value;
+};
+
+stackMethods.size = function(){
+  var keysNumber = Object.keys(storage).length;
+  return keysNumber;
+};
