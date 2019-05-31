78989b4f9e1ffeb5d1b68b99cead9cedb35a223b
Elinex <eline.ax@gmail.com>
2019-05-31 13:25:53 -0700

    Add this keyword for stack in functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,6 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
+  someInstance.storage = {};
 
   someInstance.push = stackMethods.push;
   someInstance.pop = stackMethods.pop;
@@ -12,22 +13,20 @@ var Stack = function() {
 
 var stackMethods = {};
 
-var storage = {};
-
 stackMethods.push = function(value){
   // create a numeric key
-  var keys = Object.keys(storage);
+  var keys = Object.keys(this.storage);
   var maxKey = Math.max(Number(...keys));
   var newKey = maxKey + 1;
 
   // creates a new key-value
   // add new property at the top of the stack
-  storage[newKey] = value;
+  this.storage[newKey] = value;
 };
 
 stackMethods.pop = function(){
   // get the last key in the storage
-  var keys = Object.keys(storage);
+  var keys = Object.keys(this.storage);
   var stringToNumber = function(){
     var toNumber = [];
     for (var i =0 ; i < keys.length; i ++){
@@ -39,16 +38,16 @@ stackMethods.pop = function(){
 
   // "value" is just to keep the value reference, to be returned after
   // delete storage[maxKey] happen
-  var value = storage[maxKey];
+  var value = this.storage[maxKey];
 
-  delete storage[maxKey];
+  delete this.storage[maxKey];
 
   return value;
 };
 
 stackMethods.size = function(){
   // get the number of keys in the storage
-  var keysNumber = Object.keys(storage).length;
+  var keysNumber = Object.keys(this.storage).length;
 
   return keysNumber;
 };
