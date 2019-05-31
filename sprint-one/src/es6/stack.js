39e8aa8ace553fff4ad1e762e4bedd968749b56a
Elinex <eline.ax@gmail.com>
2019-05-31 08:54:27 -0700

    Implement stack for ES6

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,35 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(){
+    this;
+    this.storage={};
   }
 
+  push(value) {
+    var keys = Object.keys(this.storage);
+    var maxKey = Math.max(Number(...keys));
+    var newKey = maxKey + 1;
+    this.storage[newKey] = value;
+  }
+
+  pop() {
+    var keys = Object.keys(this.storage);
+    var stringToNumber = function(){
+      var toNumber = [];
+      for (var i =0 ; i < keys.length; i ++){
+        toNumber.push(Number(keys[i]));
+      }
+      return toNumber;
+    }();
+    var maxKey = Math.max(...stringToNumber);
+    var value = this.storage[maxKey];
+    delete this.storage[maxKey];
+    return value;
+  }
+
+  size() {
+    var keysNumber = Object.keys(this.storage).length;
+    return keysNumber;
+  }
 }
+
+var someInstance = new Stack();
